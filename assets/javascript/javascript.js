bae0980e9e74fc34fe6d2f87ddba5fb1e298adb3
JoleenTsai <joleenjtsai@gmail.com>
2018-09-20 16:03:22 -0700

    next arrival and minutes away WORKS

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -20,13 +20,22 @@ const addTrain = _ => {
     destination: document.querySelector('#destination').value,
     firstArrival: document.querySelector('#firstArrival').value,
     frequency: document.querySelector('#frequency').value,
-    // frequency = parseInt(frequency)
+    frequency: parseInt(frequency),
   })
 }
 
 // button click to add to firebase and table
 trainAdded.on('child_added', data => {
   const train = data.val()
+
+  // math for next arrival/minutes away
+  var firstArrival = `${train.firstArrival}`;
+  var frequency = `${train.frequency}`;
+  var arrivalConverted = moment(firstArrival, "HH:mm")
+  var diffTime = moment().diff(moment(arrivalConverted), 'minutes');
+  var timeRemaining = diffTime % frequency
+  var minutesAway = frequency - timeRemaining
+  var nextTrain = moment().add(minutesAway, "minutes")
 
   // append to table
   let trainElement = document.getElementById("targetTable");
@@ -39,19 +48,9 @@ trainAdded.on('child_added', data => {
   addTrainName.innerHTML = `${train.trainName}`;
   addDestination.innerHTML = `${train.destination}`;
   addFrequency.innerHTML = `${train.frequency}`;
-  addNextArrival.innerHTML = nextTrain;
-  addMinutesAway.innerHTML = minutesAway;
+  addNextArrival.innerHTML = moment(nextTrain).format("h:mm a");
+  addMinutesAway.innerHTML = minutesAway + ' min';
 
-  var arrivalConverted = moment(firstArrival, "HH:mm").subtract(1, 'years');
-  console.log("ARRIVAL" + arrivalConverted)
-  var diffTime = moment().diff(moment(arrivalConverted), 'minutes');
-  console.log("DIFFFERNCE" + diffTime);
-  var timeRemaining = diffTime % frequency
-  console.log("TIME LEFT" + timeRemaining)
-  var minutesAway = freqGiven - timeRemaining
-  console.log("MINUTES AWAY" + minutesAway)
-  var nextTrain = moment().add(minutesAway, "minutes")
-  console.log(moment(nextTrain).format('HH:mm'))
 
   // clear text-box
   trainName = $("#trainName").val("");
